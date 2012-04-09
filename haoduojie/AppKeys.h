e8b211b0a81fbf697d8d51d7cadec140e07d655d
auscar <auscar@126.com>
2012-04-09 16:40:45 +0800

    再测试一下ignore

---
+++ haoduojie/AppKeys.h
@@ -0,0 +1,16 @@
+//
+//  AppKeys.h
+//  haoduojie
+//
+//  Created by  on 12-4-9.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef haoduojie_AppKeys_h
+#define haoduojie_AppKeys_h
+
+#define kWeiBoAppKey @"1"
+#define kWeiBoAppSecret @"2"
+
+
+#endif
